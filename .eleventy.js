d44ba52fed41cd99cc174c8db8dc5b746fef9d7a
alessandrofogliada <fogliada.a@gmail.com>
2026-09-25 13:07:03 +0200

    feat: aggiunte pagine dettaglio progetti con pagination Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-  // Copia le cartelle statiche così come sono, senza elaborarle
+  // Copia le cartelle statiche così come sono
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
@@ -8,6 +8,11 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",
       output: "_site",
+      includes: "_includes", // Specifica la cartella dei layout
+      data: "_data"          // Specifica la cartella dei dati
     },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk", // Usa Nunjucks per processare l'HTML
+    markdownTemplateEngine: "njk"
   };
 };
